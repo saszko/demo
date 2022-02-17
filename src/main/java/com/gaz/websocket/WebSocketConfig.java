6ab4598d0f6d0df9d205a27e4d37d2fdf1e64392
bulka <bulka.oleksandr@gmail.com>
2022-02-17 11:18:23 -0500

    commit

--- src/main/java/com/gaz/websocket/WebSocketConfig.java
+++ src/main/java/com/gaz/websocket/WebSocketConfig.java
@@ -14,14 +14,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         @Override
         public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/user");
-        config.setApplicationDestinationPrefixes("/esp");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
         @Override
         public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+        registry.addEndpoint("/hello")
                 .withSockJS();
     }
 }
--- src/main/java/com/gaz/websocket/WebSocketController.java
+++ src/main/java/com/gaz/websocket/WebSocketController.java
@@ -3,12 +3,20 @@ package com.gaz.websocket;
 import com.gaz.model.Condition;
 import com.gaz.service.impl.ConditionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.ConnectException;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Controller
@@ -19,18 +27,34 @@ public class WebSocketController {
     public void setConditionService(ConditionServiceImpl conditionService) {
         this.service = conditionService;
     }
+    Map<String, WebSocketSession> sessionMap = new ConcurrentHashMap<>();
 
-    @MessageMapping("/stomp")
-    public void addConditions(ReportMessage message) throws Exception {
+    @MessageMapping("/hellol")
+    public void addConditions(ReportMessage message, WebSocketSession session) throws Exception {
+        sessionMap.put(message.getControllerId(), session);
         List<Condition> conditionList = message.getConditionList()
                 .stream()
                 .map(c -> new Condition(c.getValue(), c.getMetricUnit(), c.getSensorName(), message.getControllerId()))
                 .collect(Collectors.toList());
-        //
-        // service.addAll(conditionList);
         conditionList.forEach(System.out::println);
 
+        sendMessage(message.getControllerId(), conditionList.get(0).toString());
+    }
+
+    public void sendMessage(String controllerId, String message) throws IOException {
+        WebSocketSession session = sessionMap.get(controllerId);
+        byte[] bytes = message.getBytes(message);
+        session.sendMessage(new TextMessage(bytes));
 
     }
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(TestMessage message) throws Exception {
+        Thread.sleep(3000); // simulated delay
+        return new Greeting("Hello, " + message.getMessage() + "!");
+    }
+
+
 
 }
